30a882ccb482366b8cdd7ba6668e3ca2a917578b
Abbie Gonzalez <abbie@abbiecod.es>
2012-05-01 18:07:36 -0400

    change css location
    changed css location to a style sheet for cloudflare

--- public/javascripts/dyslexia.js
+++ public/javascripts/dyslexia.js
@@ -9,7 +9,7 @@ CloudFlare.define("openDyslexic",  function () {
     
     openDyslexic.applyStyle = function () {
         var styleSheet = document.createElement('link');
-        styleSheet.setAttribute('href', 'http://dyslexicfonts.com/plugin/easyread.css');
+        styleSheet.setAttribute('href', 'http://dyslexicfonts.com/plugin/cloudflare.css');
         styleSheet.setAttribute('rel', 'stylesheet');
         styleSheet.setAttribute('type', 'text/css');
         document.getElementsByTagName('head')[0].appendChild(styleSheet);
